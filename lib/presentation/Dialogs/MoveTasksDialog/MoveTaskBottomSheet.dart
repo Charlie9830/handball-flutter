f33fe4c7f5dd1490322dec3f7aa35ef0163da658
Charlie9830 <charliewjhall@gmail.com>
2019-06-27 17:41:52 +1000

    Fixed MoveTaskBottomSheet scroll issues.

--- lib/presentation/Dialogs/MoveTasksDialog/MoveTaskBottomSheet.dart
+++ lib/presentation/Dialogs/MoveTasksDialog/MoveTaskBottomSheet.dart
@@ -23,51 +23,64 @@ class _MoveTasksBottomSheetState extends State<MoveTasksBottomSheet> {
           children: <Widget>[
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Text('Select List',
+              child: Text('Select a List',
                   style: Theme.of(context).textTheme.subhead),
             ),
-            ListView(
-              children: _getMainListChildren(),
-              shrinkWrap: true,
-              physics: AlwaysScrollableScrollPhysics(),
-            )
+            Expanded(
+              child: ListView(
+                children: _getChildren(),
+                physics: AlwaysScrollableScrollPhysics(),
+              ),
+            ),
           ],
         );
       },
     );
   }
 
-  List<Widget> _getMainListChildren() {
+  List<Widget> _getChildren() {
     var widgets = widget.taskListOptions.map((taskList) {
       return Container(
         key: Key(taskList.uid),
         child: ListTile(
           title: Text(taskList.taskListName),
-          onTap: () => _submit(MoveTaskBottomSheetResult(taskListId: taskList.uid)),
+          onTap: () =>
+              _submit(MoveTaskBottomSheetResult(taskListId: taskList.uid)),
         ),
       );
     }).toList();
 
-    widgets.insert(0, Container(
-      key: Key('new-task-list-option'),
-      child: ListTile(
-        leading: Icon(Icons.playlist_add),
-        title: Text('New List'),
-        onTap: _handleNewListOptionTap,
+    widgets.insert(
+        0,
+        Container(
+            key: Key('new-task-list-option'),
+            child: ListTile(
+              leading: Icon(Icons.playlist_add),
+              title: Text('New List'),
+              onTap: _handleNewListOptionTap,
+            )));
+    widgets.insert(
+      1,
+      Container(
+        key: Key('new-task-list-option-divider'),
+        child: Divider(),
       )
-    ));
+    );
 
     return widgets;
   }
 
   void _handleNewListOptionTap() async {
     var dialogResult = await showDialog(
-      context: context,
-      barrierDismissible: true,
-      builder: (context) => TextInputDialog(title: 'New List', text: ''));
+        context: context,
+        barrierDismissible: true,
+        builder: (context) => TextInputDialog(title: 'New List', text: ''));
 
-    if (dialogResult != null && dialogResult is TextInputDialogResult && dialogResult.result == DialogResult.affirmative) {
-      _submit(MoveTaskBottomSheetResult(isNewTaskList: true, taskListName: dialogResult.value));
+    if (dialogResult != null &&
+        dialogResult is TextInputDialogResult &&
+        dialogResult.result == DialogResult.affirmative) {
+      _submit(MoveTaskBottomSheetResult(
+          isNewTaskList: true, taskListName: dialogResult.value));
     }
   }
 
@@ -81,9 +94,6 @@ class MoveTaskBottomSheetResult {
   final bool isNewTaskList;
   final String taskListName;
 
-  MoveTaskBottomSheetResult({
-    this.taskListId,
-    this.taskListName,
-    this.isNewTaskList
-  });
+  MoveTaskBottomSheetResult(
+      {this.taskListId, this.taskListName, this.isNewTaskList});
 }
