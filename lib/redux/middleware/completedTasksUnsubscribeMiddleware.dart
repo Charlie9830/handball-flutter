b432e66628685418975dce014ddb531659c9e0c3
Charlie9830 <charliewjhall@gmail.com>
2019-07-02 00:32:16 +1000

    Fixed completedTasks Subscription falling out of sync with State.

--- lib/redux/middleware/completedTasksUnsubscribeMiddleware.dart
+++ lib/redux/middleware/completedTasksUnsubscribeMiddleware.dart
@@ -1,12 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:handball_flutter/containers/AppSettingsContainer.dart';
-import 'package:handball_flutter/containers/CommentScreenContainer.dart';
-import 'package:handball_flutter/containers/HomeScreenContainer.dart';
-import 'package:handball_flutter/containers/ShareProjectContainer.dart';
-import 'package:handball_flutter/enums.dart';
-import 'package:handball_flutter/keys.dart';
-import 'package:handball_flutter/presentation/Dialogs/TextInputDialog.dart';
-import 'package:handball_flutter/containers/TaskInspectorScreenContainer.dart';
 import 'package:handball_flutter/redux/actions.dart';
 import 'package:handball_flutter/redux/appState.dart';
 import 'package:redux/redux.dart';
@@ -16,8 +7,11 @@ void completedTasksUnsubscribeMiddleware(
   dynamic action,
   NextDispatcher next,
 ) {
-  if (action is SelectProject && store.state.showCompletedTasks == true) {
-    store.dispatch(setShowCompletedTasks(false, action.uid));
+  if (action is SelectProject &&
+      store.state.showCompletedTasks == true &&
+      store.state.selectedProjectId != '-1' &&
+      store.state.selectedProjectId != null) {
+    store.dispatch(setShowCompletedTasks(false, store.state.selectedProjectId));
   }
 
   next(action);
